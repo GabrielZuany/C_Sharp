29dc009e3f8eaf8d3bd08da061279fe3d3d4e788
GabrielZuany <gabrielzuanyduartevargas@gmail.com>
2022-12-02 11:51:50 -0300

    Uploaded src code

---
+++ 03_MSL_Introducing_the_DOTNET_Class_Library/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DOTNET_Class_Library_MSLEARN{
+
+    class Program{
+        static void Main(string[] args) {
+
+            Random program = new Random(); 
+            /*  
+                Random is a class in the .NET Class Library and stores the reference to the new object in a variable named program.
+                The new operator does several important things:
+                - It first requests an address in the computer's memory large enough to store a new object based on the Random class.
+                - It creates the new object and stores it at the memory address.
+                - It returns the memory address so that it can be saved in the program variable.
+            */
+
+            int rand = program.Next(1, 7); 
+            // calls the program object's Next() method passing in two parameters: the minimum and maximum value of the random number.
+            int rand2 = program.Next(1, 7);
+
+            int higher = Math.Max(rand, rand2); // class Math.Max() method returns the higher value.
+
+            Console.WriteLine($"n1 = {rand}, n2 = {rand2}. Higher value between these two is {higher}");
+        
+        }
+
+    }
+
+}
---
+++ 05_MSL_MultidimensionalArrays/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ARRAYS_MSLEARN{
+
+    class Program{
+        static void Main(string[] args){
+            Console.WriteLine("Building a bidimensional array...");
+            BidimensionalArray(3, 3);
+            
+            Console.WriteLine("Building a tridimensional array...");
+            TridimensionalArray(3, 3, 2);
+        }
+
+        static void BidimensionalArray(int columns, int rows){
+            int[,] arr = new int[columns, rows];
+            int columns_index, rows_index;
+
+            for(columns_index = 0; columns_index < columns; columns_index++){
+
+                for(rows_index = 0; rows_index < rows; rows_index++){
+                    arr[columns_index, rows_index] = 0;
+                    Console.Write("|" + arr[columns_index, rows_index]);
+                }
+                Console.Write("| \n");
+
+            }
+        }
+
+        static void TridimensionalArray(int columns, int rows, int tridim){
+            int[, ,] arr = new int[columns, rows, tridim];
+            int columns_index, rows_index, tridim_index;
+            int k = 3;
+
+            for(tridim_index = 0; tridim_index < tridim; tridim_index++){
+                Console.WriteLine($"Superimposed matriz index [{tridim_index}]");
+
+                for(columns_index = 0; columns_index < columns; columns_index++){
+
+                    for(rows_index = 0; rows_index < rows; rows_index++){
+                        arr[columns_index, rows_index, tridim_index] = k;
+                        Console.Write("|" + arr[columns_index, rows_index, tridim_index]);
+                    }
+                    Console.Write("| \n");
+
+                }
+                Console.WriteLine();
+                k++;
+            }
+        }
+    }
+
+}
