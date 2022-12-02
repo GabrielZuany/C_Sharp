6d11ae4b45facd923f5debfc42b2263df980dcde
GabrielZuany <gabrielzuanyduartevargas@gmail.com>
2022-12-01 23:55:30 -0300

    Uploaded src

---
+++ 01_HelloWorld/Program.cs
@@ -0,0 +1,38 @@
+﻿//modules or packages
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_HelloWorld
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             
+            Console.Write("Hello World!\n");
+            Console.WriteLine("Jump a line without '\\n'");
+
+            //var
+            int age = 20;
+            string name = "Gabriel";
+            const char genre = 'M';
+            bool value = true;
+            double height = 1.69;
+            string x = Console.ReadLine();
+            Console.WriteLine(x);
+            Console.WriteLine(age);
+            Console.WriteLine(name);
+            Console.WriteLine(value);
+            Console.WriteLine(height);
+            Console.WriteLine(genre);
+
+
+        }
+
+    }
+
+}
