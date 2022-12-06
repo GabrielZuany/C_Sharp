7259bb080916b869a32f751a0d320a5cdbf70392
GabrielZuany <gabrielzuanyduartevargas@gmail.com>
2022-12-05 21:29:09 -0300

    Upload files

---
+++ 07_MSL_Files_and_Folders/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+class Program{
+
+    static void Main(string[] args)
+    {   
+        Dir dir = new Dir();
+        string path = Directory.GetCurrentDirectory();
+    
+        dir.GetAllSubdirectories(path);
+
+        Console.Write("\n\n---------------------------------------------------\n\n");
+
+        dir.GetAllFilesInCurrentDirectory(path);
+
+        Console.Write("\n\n---------------------------------------------------\n\n");
+
+        dir.GetAllFilesInCurrentDirectoryAndSubdirectories(path);
+
+        Console.Write("\n\n---------------------------------------------------\n\n");
+
+        dir.GetCurrentFileInfo(path);
+
+        Directory.CreateDirectory(Path.Combine(path, "created_by_src"));
+    }
+
+}
