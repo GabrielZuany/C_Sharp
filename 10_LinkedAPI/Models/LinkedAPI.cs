14cd1931661e460c72799ee0ae3660eceabcbf63
GabrielZuany <gabrielzuanyduartevargas@gmail.com>
2022-12-09 16:04:00 -0300

    Upload files

---
+++ 10_LinkedAPI/Models/LinkedAPI.cs
@@ -0,0 +1,10 @@
+namespace MyLinkedAPI.Models;
+public class LinkedAPI
+{
+    public int userId { get; set; }
+    public int id { get; set; }
+    public string? title { get; set; }
+    public string? body { get; set; }
+}
+
+
