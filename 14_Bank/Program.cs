026b3bd2597c66ccef29f5f9161aaf714f25b0e9
GabrielZuany <gzuanydev@gmail.com>
2022-12-20 21:27:24 -0300

    Upload
    Studying class constructor and private atributes.

---
+++ 14_Bank/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Conta conta1 = new Conta("Alexandre", "da Silva");
+        Console.WriteLine(conta1.GetNome() + conta1.GetSobrenome());
+
+        conta1.SetNome("Gabriel");
+        conta1.SetSobrenome(" Zuany");
+        Console.WriteLine(conta1.GetNome() + conta1.GetSobrenome());
+        
+    }
+
+}
+
+public class Conta
+{
+    private string Nome;
+    private string Sobrenome;
+
+    public Conta(string Nome, string Sobrenome)//contrutor = já instancia valores na criacao do objeto.
+    {
+        this.Nome = Nome;
+        this.Sobrenome = Sobrenome;
+    }
+
+    //métodos GET/SET evitam que o usuário acesse diretamente variáveis da classe, desde que voce as instancie como private.
+    public string GetNome()
+    {
+        return Nome;
+    }
+    public string GetSobrenome()
+    {
+        return Sobrenome;
+    }
+    public void SetNome(string Nome)
+    {
+        this.Nome = Nome;
+    }
+    public void SetSobrenome(string Sobrenome)
+    {
+        this.Sobrenome = Sobrenome;
+    }
+
+}
