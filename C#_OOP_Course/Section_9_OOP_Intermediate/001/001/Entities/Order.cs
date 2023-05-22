dad913ebfc11beadbf5e66024cdb276057e27795
GabrielZuany <gzuanydev@gmail.com>
2023-05-22 11:11:57 -0300

    Course files uploaded

---
+++ C#_OOP_Course/Section_9_OOP_Intermediate/001/001/Entities/Order.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using _001.Entities.Enums;
+
+namespace _001.Entities
+{
+    internal class Order
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public OrderStatus Status { get; set; }
+
+        public override string ToString()
+        {
+            return $"Id: {Id}, Date: {Date}, Status: {Status}";
+        }
+    }
+}
--- C#_OOP_Course/Section_9_OOP_Intermediate/001/001/Program.cs
+++ C#_OOP_Course/Section_9_OOP_Intermediate/001/001/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using _001.Entities;
+using _001.Entities.Enums;
+
+namespace _001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order
+            {
+                Id = 1080,
+                Date = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+            Console.WriteLine(order);
+
+            // Convert enum to string
+            string txt = OrderStatus.PendingPayment.ToString();
+            Console.WriteLine(txt);
+
+            // Convert string to enum
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+            Console.WriteLine(os);
+        }
+    }
+}
