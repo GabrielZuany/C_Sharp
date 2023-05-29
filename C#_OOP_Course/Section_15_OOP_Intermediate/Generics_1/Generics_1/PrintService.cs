bde169a6db05ed2674a403e84adec681fd6660ac
GabrielZuany <gzuanydev@gmail.com>
2023-05-29 11:09:36 -0300

    Course files uploaded

---
+++ C#_OOP_Course/Section_15_OOP_Intermediate/Generics_1/Generics_1/PrintService.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Generics_1
+{
+    internal class PrintService<T>
+    {
+        private T[] _values = new T[10];
+        private int _count = 0;
+
+        public void add(T value)
+        {
+            if (_count == _values.Length)
+            {
+                throw new InvalidOperationException("PrintService is full");
+            }
+            _values[_count] = value;
+            _count++;
+        }
+
+        public T first()
+        {
+            if (_count == 0)
+            {
+                throw new InvalidOperationException("PrintService is empty");
+            }
+            return _values[0];
+        }
+
+        public void print()
+        {
+            Console.Write("[");
+            for (int i = 0; i < _count - 1; i++)
+            {
+                Console.Write(_values[i] + ", ");
+            }
+            if (_count > 0)
+            {
+                Console.Write(_values[_count - 1]);
+            }
+            Console.WriteLine("]");
+        }
+
+        public T max<T>(List<T> list) where T : IComparable
+        {
+            if (list.Count == 0)
+            {
+                throw new ArgumentException("Empty list");
+            }
+            T max = list[0];
+            for (int i = 1; i < list.Count; i++)
+            {
+                if (list[i].CompareTo(max) > 0)
+                {
+                    max = list[i];
+                }
+            }
+            return max; 
+        }
+
+    }
+}
---
+++ C#_OOP_Course/Section_15_OOP_Intermediate/Generics_1/Generics_1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Generics_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            PrintService<int> ps = new PrintService<int>();
+            Console.Write("How many values? ");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+                ps.add(x);
+            }
+            ps.print();
+            Console.WriteLine("First: " + ps.first());
+
+
+            PrintService<string> ps2 = new PrintService<string>();
+            Console.Write("How many values? ");
+            n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string x = Console.ReadLine();
+                ps2.add(x);
+            }
+            ps2.print();
+            Console.WriteLine("First: " + ps2.first());
+        }
+    }
+}
