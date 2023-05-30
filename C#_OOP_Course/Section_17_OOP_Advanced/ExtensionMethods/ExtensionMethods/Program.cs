b9484b904c0cd32f77093e033292250a2183c4f1
GabrielZuany <gzuanydev@gmail.com>
2023-05-30 08:58:04 -0300

    Course files uploaded

--- C#_OOP_Course/Section_17_OOP_Advanced/ExtensionMethods/ExtensionMethods/Program.cs
+++ C#_OOP_Course/Section_17_OOP_Advanced/ExtensionMethods/ExtensionMethods/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ExtensionMethods.Extensions;
+using System;
+using System.Collections.Generic;
+
+namespace ExtensionMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = new DateTime(2022, 11, 16, 8, 10, 15);
+            Console.WriteLine(dt.ElapsedTime());
+        }
+    }
+}
