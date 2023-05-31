366adfd8a87ea717af7615c676177ce922d069f0
GabrielZuany <gzuanydev@gmail.com>
2023-05-30 22:18:55 -0300

    Course files uploaded

---
+++ C#_OOP_Course/Section_17_OOP_Advanced/Delegates/Delegates/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Delegates.Services;
+
+namespace Delegates
+{
+    delegate double BinaryNumericOperation(double n1, double n2);
+    class Program
+    {
+        delegate double BinaryNumericOperation(double n1, double n2);
+
+        static void Main(string[] args)
+        {
+            double a = 10;
+            double b = 12;
+
+            BinaryNumericOperation op = CalculationService.Sum;
+            double result = op(a, b);
+            Console.WriteLine("Using delegate(sum): " + result);
+            
+        }
+    }
+}
