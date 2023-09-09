c3f525a08d5df4df933da9bc78e54dc9f0b8926b
GabrielZuany <gzuanydev@gmail.com>
2023-09-08 23:37:23 -0300

    Project uploaded

---
+++ EntityFramework/mslearn-persist-data-ef-core/ContosoPizza/Data/PromotionsContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ContosoPizza.Models;
+
+namespace ContosoPizza.Data
+{
+    public class PromotionsContext : DbContext
+    {
+        public PromotionsContext(DbContextOptions<PromotionsContext> options)
+            : base(options)
+        {
+        }
+        // The DbSet<T> properties correspond to tables to create in the database.
+        public DbSet<Coupon> Coupons => Set<Coupon>();
+    }
+}
