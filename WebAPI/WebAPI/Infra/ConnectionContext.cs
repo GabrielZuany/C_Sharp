4a6c5c648e027c6988492233cbc54875209f886a
GabrielZuany <gzuanydev@gmail.com>
2024-03-28 12:34:42 -0300

    init db and db connection

---
+++ WebAPI/WebAPI/Infra/ConnectionContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAPI.Model;
+
+namespace WebAPI.Infra
+{
+    public class ConnectionContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; } // map the Employee class to the employee table        
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=localhost;Port=5434;Database=webapi;Username=webapi;Password=webapi");
+        }
+
+
+    }
+}
---
+++ WebAPI/WebAPI/PostgreSQL/populating.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE employee (
+	id uuid PRIMARY KEY NOT NULL,
+    fullname varchar(50) NOT NULL,
+    age int NOT NULL,
+    postcode varchar(8)
+	);
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+INSERT INTO employee (id, fullname, age, postcode) VALUES(gen_random_uuid(), 'test_user', 25, 'xxxxxxxx');
