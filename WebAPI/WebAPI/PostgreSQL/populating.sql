a16e8a4d8df564340aff38025012ec4bcb6b8e3e
GabrielZuany <gzuanydev@gmail.com>
2024-03-29 14:41:36 -0300

    add column

--- WebAPI/WebAPI/PostgreSQL/populating.sql
+++ WebAPI/WebAPI/PostgreSQL/populating.sql
@@ -3,9 +3,8 @@
 	    id uuid PRIMARY KEY NOT NULL,
         fullname varchar(50) NOT NULL,
         age int NOT NULL,
-        postcode varchar(8)
+        postalcode varchar(8),
+        photo varchar(255)
 	    );
     CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-
-    INSERT INTO employee (id, fullname, age, postcode) VALUES(gen_random_uuid(), 'test_user', 25, 'xxxxxxxx');
 */
