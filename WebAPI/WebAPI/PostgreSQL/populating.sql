571559f0b8b9b51d4a56d37da3652590ccfbbad5
GabrielZuany <gzuanydev@gmail.com>
2024-03-29 01:03:57 -0300

    code supressed

--- WebAPI/WebAPI/PostgreSQL/populating.sql
+++ WebAPI/WebAPI/PostgreSQL/populating.sql
@@ -1,9 +1,11 @@
-﻿CREATE TABLE employee (
-	id uuid PRIMARY KEY NOT NULL,
-    fullname varchar(50) NOT NULL,
-    age int NOT NULL,
-    postcode varchar(8)
-	);
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+﻿/*
+    CREATE TABLE employee (
+	    id uuid PRIMARY KEY NOT NULL,
+        fullname varchar(50) NOT NULL,
+        age int NOT NULL,
+        postcode varchar(8)
+	    );
+    CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-INSERT INTO employee (id, fullname, age, postcode) VALUES(gen_random_uuid(), 'test_user', 25, 'xxxxxxxx');
+    INSERT INTO employee (id, fullname, age, postcode) VALUES(gen_random_uuid(), 'test_user', 25, 'xxxxxxxx');
+*/
